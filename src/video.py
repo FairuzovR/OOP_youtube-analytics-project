859ee29947d4e4537fc85f490b6ad57af817feb0
FAIRU <skyfairrr@gmail.com>
2024-02-22 00:54:18 +0500

    finished HomeWork 6 -2

--- src/video.py
+++ src/video.py
@@ -1,20 +1,27 @@
 from src.channel import Channel
 from googleapiclient.discovery import build
 import os
+
 class Video:
 
     api_key = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',id=self.video_id).execute()
-        self.url = f"https://www.youtube.com/chanell/{self.video_id}"
-        self.title = self.video_response['items'][0]['snippet']["title"]
-        self.viewcount = self.video_response['items'][0]['statistics']['viewCount']
-        self.likecount = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails',id=self.video_id).execute()
+            self.url = f"https://www.youtube.com/chanell/{self.video_id}"
+            self.title = self.video_response['items'][0]['snippet']["title"]
+            self.viewcount = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_response = None
+            self.url = None
+            self.title = None
+            self.viewcount = None
+            self.like_count = None
     def __str__(self):
 
         return f'{self.title}'
